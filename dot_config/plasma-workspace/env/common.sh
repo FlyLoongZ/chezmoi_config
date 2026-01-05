1aef03d64ff286ea96874438d0bf7e9efe76ca60
FlyLoongZ <60963681+FlyLoongZ@users.noreply.github.com>
2026-01-05 19:17:01 +0800

    删除多余环境变量

--- dot_config/plasma-workspace/env/common.sh
+++ dot_config/plasma-workspace/env/common.sh
@@ -1,5 +1,4 @@
 export XMODIFIERS=@im=fcitx
-export GTK_USE_PORTAL=1
 
 # xdg env
 export XDG_CACHE_HOME=$HOME/.cache
@@ -8,12 +7,10 @@ export XDG_DATA_HOME=$HOME/.local/share
 export XDG_STATE_HOME=$HOME/.local/state
 
 # xdg setting for app
-export VDPAU_DRIVER=radeonsi
 export ANDROID_USER_HOME="$XDG_DATA_HOME"/android
 export CARGO_HOME="$XDG_DATA_HOME"/cargo
 export DOTNET_CLI_HOME="$XDG_DATA_HOME"/dotnet
 export GOPATH="$XDG_DATA_HOME"/go
-export GTK2_RC_FILES="$XDG_CONFIG_HOME"/gtk-2.0/gtkrc
 
 # ANDROID DEV ENV
 export ANDROID_HOME="$HOME/Android/Sdk"
